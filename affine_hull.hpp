a12d418b9d6704b9c425650a71dd6ab489ab3d10
Lars Kühne <lars.kuehne@uni-jena.de>
2013-10-02 15:08:44 +0200

    finished affine_hull class and added its test case

--- affine_hull.hpp
+++ affine_hull.hpp
@@ -4,44 +4,61 @@
 #include <cassert>
 
 #include <algorithm>
+#include <iterator>
 #include <vector>
+
+#include <Eigen/Dense>
 
 #include "point_cloud.hpp"
 #include "dynamic_qr.hpp"
 
 namespace fc {
 
+/**
+  @brief supports d + 1 points, where d is the dimension
+*/
 template <typename _number_type, typename _size_type, typename _dim_type>
 class affine_hull {
   using member_container = std::vector<_size_type>;
+  using eigen_vector = Eigen::Matrix<_number_type, Eigen::Dynamic, 1>;
+  using eigen_cmap = Eigen::Map<const eigen_vector>;
 
-  public:  
+  public:
+    typedef _number_type number_type;
     typedef _size_type size_type;
-    typedef member_container::const_iterator const_iterator;
+    typedef _dim_type dim_type;
+    typedef typename member_container::const_iterator const_iterator;
     typedef point_cloud<number_type, dim_type, size_type> point_cloud_t;
     
-    affine_hull(point_cloud_t const& pc) : _pc(pc) {
+    affine_hull(point_cloud_t const& pc) : _pc(pc), _dyn_qr(pc.dim()) {
     }
     
     void add_point(size_type const idx) {
       assert(!is_member(idx));
-      if (size() > 0) {
-        // TODO append qr-dolumn
-      }
+      assert(size() <= _pc.dim());
+      // it requires an idx that serves as the origin for adding a column
+      if (size() > 0)
+        _dyn_qr.append_column(eigen_cmap(_pc[idx], _pc.dim()) -
+                              eigen_cmap(_pc[_members.front()], _pc.dim()));
       _members.push_back(idx);
-      
     }
     
     void drop_point(size_type const idx) {
       assert(is_member(idx));
-      auto it = std::find(_members.begin(), _members.end(), idx);
-      if (_members.begin() == it && _members.size() > 1) {
-        // TODO rank-1 update
-      } else if (_members.size() > 1) {
-        // TODO regular column drop
+      auto const it = std::find(_members.begin(), _members.end(), idx);
+      if (_members.size() > 1) {
+        bool const del_orig = _members.begin() == it;
+        _dyn_qr.delete_column(del_orig ? 0 :
+                              std::distance(_members.begin(), it) - 1);
+        if (_members.size() > 2 && del_orig) {
+          _dyn_qr.rank_one_update(eigen_cmap(_pc[_members.front()], _pc.dim()) -
+                                  eigen_cmap(_pc[_members[1]], _pc.dim()),
+                                  eigen_vector::Ones(_dyn_qr.num_cols()));
+        }
       }
-      
       _members.erase(it);
+      assert((_members.empty() && _dyn_qr.num_cols() == 0) ||
+             _members.size() == _dyn_qr.num_cols() + 1u);
     }
     
     /**
@@ -64,7 +81,10 @@ class affine_hull {
       @param lambda pointer to the vector of projection coefficients
     */
     void project(number_type const* x, number_type * lambda) {
-      // TODO
+      assert(size() > 0);
+      if (_members.size() > 1)
+        _dyn_qr.solve(x, lambda + 1);
+      *lambda = 1 - eigen_cmap(lambda + 1, _dyn_qr.num_cols()).sum();
     }
   
   private:
@@ -72,9 +92,9 @@ class affine_hull {
       return _members.end() != std::find(_members.begin(), _members.end(), idx);
     }
 
-    point_cloud_t const& _pc;  
+    point_cloud_t const& _pc;
+    dynamic_qr<_number_type, _dim_type> _dyn_qr;
     std::vector<_size_type> _members;
-    dynamic_qr<_number_type> _dyn_qr;  // TODO: here
 };
 
 }  // namespace fc
