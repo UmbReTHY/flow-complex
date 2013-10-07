9c35c2fd6f267b1116edf60b4262f5f315a5324d
Lars Kühne <lars.kuehne@uni-jena.de>
2013-10-07 16:16:17 +0200

    push commit

--- nn_along_ray.hpp
+++ nn_along_ray.hpp
@@ -3,6 +3,7 @@
 
 #include <cassert>
 
+#include <iterator>
 #include <stdexcept>
 #include <utility>
 #include <vector>
@@ -18,11 +19,13 @@ template <typename number_type, typename size_type, typename dim_type,
 std::pair<number_type, std::vector<size_type>>
 nearest_neighbor_along_ray(number_type const* loc,
                            number_type const* ray,
-                           point_cloud<number_type, size_type, dim_type> const& pc, // TODO templ templ
-                           iterator members_begin, iterator members_end,
-                           size_type const p_idx) {
+                           point_cloud<number_type,
+                                       size_type, dim_type> const& pc,
+                           iterator members_begin, iterator members_end) {
   using eigen_vector = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
   using eigen_cmap = Eigen::Map<const eigen_vector>;
+  assert(std::distance(members_begin, members_end) > 0);
+  auto const p_idx = *members_begin;
   eigen_cmap v(ray, pc.dim());
   eigen_cmap p(pc[p_idx], pc.dim());
   eigen_cmap x(loc, pc.dim());
@@ -36,7 +39,7 @@ nearest_neighbor_along_ray(number_type const* loc,
     if (members_end == std::find(members_begin, members_end, i)) {
       eigen_cmap q(pc[i], pc.dim());
       number_type const tmp = (v_p - q.dot(v));
-      if (tmp.array().all() == 0)
+      if (tmp == 0)
         throw std::runtime_error("ill-conditioned data: div by 0");
       number_type const t = (2 * (x.dot(q) - x_p) + p_p - q.dot(q)) /
                             (2 * tmp);
--- tests/nn_along_ray.cpp
+++ tests/nn_along_ray.cpp
@@ -28,33 +28,34 @@ using eigen_vector = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
 
 using namespace fc;
 
-eigen_matrix points;
+eigen_matrix points(DIM, NUM_PTS);
 eigen_vector loc;
-std::vector<size_type> members;
+std::vector<size_type> members(NUM_MEMBERS);
+std::vector<number_type const*> ptr_cont(NUM_PTS);
 
 void generate_data() {
   number_type const EMPTY_BALL_RADIUS = number_type(1.0);
   number_type const MAX_RADIUS = number_type(10.0);
   static_assert(MAX_RADIUS > EMPTY_BALL_RADIUS, "");
-  eigen_matrix points(DIM, NUM_PTS);
-  std::vector<size_type> members;
   // generate members at random on sphere
   size_type i;
   for (i = 0; i < NUM_MEMBERS; ++i) {
-    members.push_back(i);
+    members[i] = i;
     eigen_vector const tmp = eigen_vector::Random(DIM);
     points.col(i) = tmp * (EMPTY_BALL_RADIUS / tmp.norm());
+    ptr_cont[i] = points.col(i).data();
   }
   
   // generate rest of pts at random outside the same sphere
-    for (; i < NUM_PTS; ++i) {
-      eigen_vector const tmp = eigen_vector::Random(DIM);
-      auto const rnd_num = std::rand() / static_cast<number_type>(RAND_MAX);
-      // all other pts should be outside the member sphere
-      auto const small_offset = 1e-5;
-      auto const radius = (EMPTY_BALL_RADIUS + small_offset) +
-                          (MAX_RADIUS - EMPTY_BALL_RADIUS) * rnd_num;
-      points.col(i) = tmp * (radius / tmp.norm());
+  for (; i < NUM_PTS; ++i) {
+    eigen_vector const tmp = eigen_vector::Random(DIM);
+    auto const rnd_num = std::rand() / static_cast<number_type>(RAND_MAX);
+    // all other pts should be outside the member sphere
+    auto const small_offset = 1e-5;
+    auto const radius = (EMPTY_BALL_RADIUS + small_offset) +
+                        (MAX_RADIUS - EMPTY_BALL_RADIUS) * rnd_num;
+    points.col(i) = tmp * (radius / tmp.norm());
+    ptr_cont[i] = points.col(i).data();
   }
   
   // select the location and shift all points to that origin
@@ -62,22 +63,25 @@ void generate_data() {
   points += loc.replicate(1, NUM_PTS);
 }
 
+bool is_valid(number_type const t,
+              std::vector<size_type> const members) {
+  // TODO continue here
+}
+
 int main() {
   std::srand(std::time(nullptr));
-  generate_data();
-  std::vector<number_type const*> ptr_cont;
-  for (size_type i = 0; i < pt_src.cols(); ++i)
-    ptr_cont.push_back(pt_src.col(i).data());
-
-  point_cloud<number_type, size_type, dim_type> pc(DIM, ptr_cont.begin(),
-                                                        ptr_cont.end());
   
-  for (size_type i = 0; i < NUM_TRIALS; ++i) {
-    eigen_vector ray = eigen_vector::Random(DIM);
-    auto const res_pair =
-    nearest_neighbor_along_ray(loc.data(), ray.data(), );
+  eigen_vector ray;
+  size_type i = 0;
+  do {
     generate_data();
-  }
+    point_cloud<number_type, size_type, dim_type> pc(DIM, ptr_cont.begin(),
+                                                          ptr_cont.end());
+    ray = eigen_vector::Random(DIM);
+    auto const res_pair =
+    nearest_neighbor_along_ray(loc.data(), ray.data(), pc,
+                               members.begin(), members.end());
+  } while(++i < NUM_TRIALS);
   
   std::exit(EXIT_SUCCESS);
 }
