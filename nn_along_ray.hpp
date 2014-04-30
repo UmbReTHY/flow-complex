394d15a009210d0a625deee75cbe0b95b11358f8
Lars Kühne <lars.kuehne@uni-jena.de>
2014-04-30 15:08:16 +0200

    changed the nn_along ray routine to use exceptions and support mulitple nearest neighbors

--- nn_along_ray.hpp
+++ nn_along_ray.hpp
@@ -2,41 +2,31 @@
 #define NN_ALONG_RAY_HPP_
 
 #include <cassert>
-#include <cstdint>
 #include <cstddef>
 
 #include <limits>
-#include <tuple>
+#include <pair>
+#include <stdexcept>
 
 #include <Eigen/Core>
 
-#include "point_cloud.hpp"
-
 namespace FC {
-
-enum class NN_ERROR : std::uint8_t {
-  OK,
-  DIV_BY_ZERO,  // solved by perturbing data
-  MORE_THAN_ONE_NN  // solved by perturbing data
-};
 
 /**
   @tparam F callable taking a pointer to std::size_t and returning a pointer
             to an eigen_map
-  @param loc point with the current location
-  @param pc the point cloud
-  @param p_idx index of a point of the point cloud, which is at starting
-               distance from loc
-  @param filter function that, given an index into pc, returns wheter or not
-                this point does even need to be considered as a candidate
+  @param begin, end iterators iterators for nearest neighbor storage.
+                    If the number of nearest neighbors found exceeds
+                    the given range, an exception is thrown.
 */
 template <typename Derived1, typename Derived2, typename Derived3,
-          typename F>
-std::tuple<std::size_t, typename Derived1::Scalar, NN_ERROR>
+          typename F, typename NNIterator>
+std::pair<NNIterator, typename Derived1::Scalar>
 nearest_neighbor_along_ray(Eigen::MatrixBase<Derived1> const& x,
                            Eigen::MatrixBase<Derived2> const& v,
                            Eigen::MatrixBase<Derived3> const& p,
-                           F & get_next) {
+                           F & get_next,
+                           NNIterator begin, NNIterator end) {
   using number_type = typename Derived1::Scalar;
   static_assert(std::numeric_limits<number_type>::has_infinity,
                 "number_type needs to have a representation for infinity");
@@ -45,27 +35,24 @@ nearest_neighbor_along_ray(Eigen::MatrixBase<Derived1> const& x,
   number_type const p_p = p.dot(p);
   number_type const v_p = v.dot(p);
   // init return value
-  auto r = std::make_tuple(std::size_t(0),
-                           std::numeric_limits<number_type>::infinity(),
-                           NN_ERROR::OK);
+  auto r = std::make_pair(begin, std::numeric_limits<number_type>::infinity());
   std::size_t q_idx;
   auto * q_ptr = get_next(&q_idx);
   while (q_ptr) {
     auto & q = *q_ptr;
     number_type const tmp = (v_p - q.dot(v));
-    if (0 == tmp) {
-      std::get<2>(r) = NN_ERROR::DIV_BY_ZERO;
-      break;
-    }
+    if (0 == tmp)
+      throw std::runtime_error("division by 0");
     number_type const t = (x.dot(q) - x_p + 0.5 * (p_p - q.dot(q))) / tmp;
-    if (t > 0) {
-      if (t < std::get<1>(r)) {
-        std::get<0>(r) = q_idx;
-        std::get<1>(r) = t;
-      } else if (t == std::get<1>(r)) {
-        std::get<2>(r) = NN_ERROR::MORE_THAN_ONE_NN;
-        break;
+    if (t > 0 and t <= r.second) {
+      if (t == r.second) {
+        if (r.first == end)
+          throw std::runtime_error("too many nearest neighbors");
+      } else {
+        r.first = begin;
+        r.second = t;
       }
+      *r.first++ = q_idx;
     }
     q_ptr = get_next(&q_idx);
   }
