e65ab69d548c03626260adbfc385743cb225fda2
Lars Kühne <lars.kuehne@uni-jena.de>
2014-04-30 15:17:35 +0200

    removed ray from constructor to reduce size

--- descend_task.hpp
+++ descend_task.hpp
@@ -5,41 +5,41 @@
 
 #include <Eigen/Dense>
 
+#include "critical_point.hpp"
 #include "affine_hull.hpp"
+#include "update_ray.hpp"
 
 namespace FC {
 
-// forward declaration
-template <typename point_cloud_t>
-class ascend_task;
-
 template <typename point_cloud_t>
 class descend_task {
-  using point_cloud_type = point_cloud_t;
-  using number_type = typename point_cloud_type::number_type;
-  using eigen_vector = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
-  
 public:
-  typedef point_cloud_t point_cloud_type;
-
+  typedef point_cloud_t                          point_cloud_type;
+  typedef typename point_cloud_type::number_type number_type;
+  typedef typename point_cloud_type::size_type   size_type;
+private:
+  using eigen_vector = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
+public:
   descend_task(affine_hull<point_cloud_type> && ah,
                eigen_vector && location,
-               eigen_vector && ray) : _ah(std::move(ah)) {
+               critical_point<number_type, size_type> * succ)
+    : _ah(std::move(ah)), _succ(succ) {
     _location.swap(location);
-    _ray.swap(ray);
   }
   
   template <typename DTHandler, typename ATHandler, typename CPHandler>
   void execute(DTHandler & dth, ATHandler & ath, CPHandler & cph) {
+    auto const& pc = _ah.pc();
+    eigen_vector ray;
+    eigen_vector driver(pc.dim());
+    eigen_vector lambda(pc.dim() + 1);
+    update_ray(_ah, _location, lambda, driver, ray);
   }
   
 private:
-  affine_hull<point_cloud_type> _ah;
-  eigen_vector                  _location;
-  eigen_vector                  _ray;
-  // TODO remove this: better provide a ascend_task-constructor
-  //                   that directly takes the required arguments
-  friend class ascend_task<point_cloud_type>;  
+  affine_hull<point_cloud_type>            _ah;
+  eigen_vector                             _location;
+  critical_point<number_type, size_type> * _succ;
 };
 
 }  // namespace FC
