868c67aa9eaf85305aed575014c83edc114a00f3
Lars Kühne <lars.kuehne@uni-jena.de>
2014-07-07 11:26:29 +0200

    header cleaning and replacing std::cout with Logger()

--- common.hpp
+++ common.hpp
@@ -3,15 +3,18 @@
 
 #include <cassert>
 
-#include <iostream>
+#include <algorithm>
+#include <ostream>
 #include <iterator>
+#include <utility>
 
 #include <Eigen/Core>
 
-#include "descend_task.hpp"
-#include "ascend_task.hpp"
 #include "affine_hull.hpp"
+#include "ascend_task.hpp"
 #include "critical_point.hpp"
+#include "descend_task.hpp"
+#include "logger.hpp"
 #include "update_ray.hpp"
 
 namespace FC {
@@ -31,7 +34,6 @@ template <class Derived, class Iterator>
 Iterator get_pos_offsets(Eigen::MatrixBase<Derived> const& lambda,
                          Iterator pos_begin) {
   using number_t = typename Eigen::MatrixBase<Derived>::Scalar;
-  // TODO put stability threshold here
   return get_cond_offsets(lambda, pos_begin, [](number_t a) {return a > 0;});
 }
 
@@ -39,16 +41,19 @@ template <class Derived, class Iterator>
 Iterator get_neg_offsets(Eigen::MatrixBase<Derived> const& lambda,
                          Iterator neg_begin) {
   using number_t = typename Eigen::MatrixBase<Derived>::Scalar;
-  // TODO put stability threshold here
   return get_cond_offsets(lambda, neg_begin, [](number_t a) {return a < 0;});
 }
 
 /**
-  @brief schedules descend tasks for every point but one that is dropped in
-         the range. One point is dropped on ah directly and is intended to be
-         reused in other tasks instead.
-  @param num_dropped the number of points for which to schedule a descend task
-                     after being dropped, starting at ah.begin()
+  @brief Spawns descend tasks for every sub-simplex of ah that is obtained
+         by dropping an index from the given range of positive indices.
+  @param dth Function object that spawns the given descend_task
+  @param drop_pos_begin, drop_pos_end iterators over the position offsets from
+                                      ah.begin() that shall be dropped
+  @param x the starting position of the yet to spawn descend tasks
+  @param ah the simplex to drop from
+  @param succ the pointer of the successor in the Hasse-diagram for the new
+              descend tasks
 */
 template <class DTHandler, class PointCloud, typename number_type,
           typename size_type, class Iterator>
@@ -67,13 +72,13 @@ void spawn_sub_descends(DTHandler & dth,
     auto new_ah(ah);
     new_ah.drop_point(new_ah.begin() + *it);
     auto const& dropped_idx = *(ah.begin() + *it);
-    std::cout << "DT takes dropped idx = " << dropped_idx << std::endl;
+    Logger() << "DT takes dropped idx = " << dropped_idx << std::endl;
     using eigen_vector = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
     dth(dt(std::move(new_ah), eigen_vector(x), succ, dropped_idx));
   }
   assert(drop_pos_begin != drop_pos_end);
   size_type dropped_idx = *(ah.begin() + *drop_pos_begin);
-  std::cout << "DT takes dropped idx = " << dropped_idx << std::endl;
+  Logger() << "DT takes dropped idx = " << dropped_idx << std::endl;
   ah.drop_point(ah.begin() + *drop_pos_begin);
   dth(dt(std::move(ah), std::move(x), succ, dropped_idx));
 }
@@ -96,14 +101,14 @@ void simplex_case_upflow(affine_hull<PointCloud> ah,
   assert(ah.size() == (pc.dim() + 1));
   assert(std::distance(begin, end) >= pc.dim() + 1);
   // TODO static_assert: type of IdxIterator == size_type
-  std::cout << "FINITE MAX SUSPECT\n";
+  Logger() << "FINITE MAX SUSPECT\n";
   assert(lambda.size() == ah.size());
   ah.project(x, lambda);
-  std::cout << "lambda = " << lambda.head(ah.size()).transpose() << std::endl;
+  Logger() << "lambda = " << lambda.head(ah.size()).transpose() << std::endl;
   auto const neg_end = get_neg_offsets(lambda, begin);
   if (begin == neg_end) {  // no points negative
-    std::cout << "FINITE MAX INDEED\n";
-    std::cout << "MAX-LOC " << x.transpose() << std::endl;
+    Logger() << "FINITE MAX INDEED\n"
+             << "MAX-LOC " << x.transpose() << std::endl;
     number_type sq_dist((x - pc[*ah.begin()]).squaredNorm());
     using cp_type = critical_point<number_type, size_type>;
     auto r_pair = cph(cp_type(ah.begin(), ah.end(), sq_dist));
@@ -121,7 +126,7 @@ void simplex_case_upflow(affine_hull<PointCloud> ah,
                          max_ptr);
     }
   } else {
-    std::cout << "NO FINITE MAX - SPAWN NEW ASCEND TASKS\n";
+    Logger() << "NO FINITE MAX - SPAWN NEW ASCEND TASKS\n";
     size_type dropped_idx;  // TODO get rid of this temporary
     // TODO move the last iteration rather than copying it
     for (auto it = begin; it != neg_end; ++it) {
@@ -131,8 +136,8 @@ void simplex_case_upflow(affine_hull<PointCloud> ah,
       update_ray<RAY_DIR::FROM_DRIVER>(new_ah, x, lambda, driver, ray);
       using ev = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
       using at = ascend_task<PointCloud>;
-      std::cout << "NEW TASK: " << new_ah << std::endl;
-      std::cout << "DROPPED IDX = " << dropped_idx << std::endl;
+      Logger() << "NEW TASK: " << new_ah << std::endl
+               << "DROPPED IDX = " << dropped_idx << std::endl;
       ath(at(std::move(new_ah), ev(x), ev(ray), dropped_idx));
     }
   }
