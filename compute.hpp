f6c9f6249dc6c9c577f04f99e41a7d0b8034e390
Lars Kühne <lars.kuehne@uni-jena.de>
2014-08-01 16:36:08 +0200

    the number of threads used to compute the flow complex is now fixed to the maximum number available

--- compute.hpp
+++ compute.hpp
@@ -23,13 +23,11 @@ template <typename size_type,    // type that is capable of holding the indices
                                  // of the critical points
           bool Aligned = false,
           typename PointIterator,
-          typename dim_type,
-          typename num_threads_t>
+          typename dim_type>
 flow_complex<typename base_t<decltype((*PointIterator())[0])>::type,
              size_type>
 compute_flow_complex (PointIterator begin, PointIterator end,
-                      dim_type dim,
-                      num_threads_t /*num_threads*/ num_threads = 1) {
+                      dim_type dim) {
   Logger() << "*****************COMPUTE-START*************************\n";
   using number_type = typename base_t<decltype((*PointIterator())[0])>::type;
   using fc_type = flow_complex<number_type, size_type>;
@@ -56,10 +54,11 @@ compute_flow_complex (PointIterator begin, PointIterator end,
   using item_t = std::shared_ptr<abstract_task<pc_type>>;
   using feeder_t = tbb::parallel_do_feeder<item_t>;
   std::vector<item_t> tasks;
-  for (num_threads_t i = 1; i <= num_threads; ++i)
+  tbb::task_scheduler_init init;
+  int num_threads = init.default_num_threads();
+  for (int i = 1; i <= num_threads; ++i)
     tasks.push_back(item_t(make_task_wrapper(at_type(pc), acih)));
   // 4) process all tasks
-  tbb::task_scheduler_init init(num_threads);
   tbb::parallel_do(tasks.begin(), tasks.end(), [&](item_t item, feeder_t & f) {
     using ttb_at_t = tbb_task_wrapper<at_type, decltype(acih)>;
     using ttb_dt_t = tbb_task_wrapper<dt_type, decltype(dcih)>;
