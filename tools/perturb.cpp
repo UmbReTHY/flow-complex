4069cc32d2dc267c74dca503df853fe2225dda7a
Lars Kühne <lars.kuehne@uni-jena.de>
2014-08-06 15:39:56 +0200

    perturb tool now uses static perturbation radius for all points

--- tools/perturb.cpp
+++ tools/perturb.cpp
@@ -12,38 +12,33 @@
 #include <Eigen/Core>
 
 #include "file_io.hpp"
-#include "point_cloud.hpp"
 
 int main(int argc, char ** argv) {
 try {
-  if (3 != argc)
-    throw std::invalid_argument("usage: perturb <in_file> <out_file>");
+  if (4 != argc)
+    throw std::invalid_argument("usage: perturb <eps> <in_file> <out_file>");
   // read in_file
   using float_t = double;
-  char const* in_filename = argv[1];
+  char const* in_filename = argv[2];
   std::ifstream in_file(in_filename);
   if (not in_file)
     throw std::runtime_error(std::string("could not open ") + in_filename);
   FC::point_store<float_t> ps;
   in_file >> ps;
   // perform pertubation
+  float_t const FACTOR = std::atof(argv[1]);
   if (ps.size()) {
     using size_type = std::size_t;
-    auto pc = FC::point_cloud<float_t, size_type, false>(ps.cbegin(), ps.cend(),
-                                                         ps[0].size());
-    float_t const FACTOR = 0.1;
     std::vector<size_type> nn(2);
     std::vector<float_t> sq_dists(2);
     for (auto & pv : ps) {
       using EigenVector = Eigen::Matrix<float_t, Eigen::Dynamic, 1>;
-      auto p = typename EigenVector::MapType(pv.data(), pc.dim());
-      pc.k_nearest_neighors(p, 2, nn.data(), sq_dists.data());
-      auto dist = sq_dists[1];
-      p += (FACTOR * dist) * EigenVector::Random(pc.dim()).normalized();
+      auto p = typename EigenVector::MapType(pv.data(), pv.size());
+      p += FACTOR * EigenVector::Random(pv.size()).normalized();
     }
   }
   // write out_file
-  char const* out_filename = argv[2];
+  char const* out_filename = argv[3];
   std::ofstream out_file(out_filename);
   if (not out_filename)
     throw std::runtime_error(std::string("could not open ") + out_filename);
