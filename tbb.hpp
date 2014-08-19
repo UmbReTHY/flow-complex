172f08c0b659b8428caa401e07658cda73edaa43
Lars Kuehne <lars.kuehne@uni-jena.de>
2014-08-19 12:57:59 +0200

    Compound task that allows control over task granularity for better scalability

--- tbb.hpp
+++ tbb.hpp
@@ -1,7 +1,7 @@
 #ifndef TBB_HPP_
 #define TBB_HPP_
 
-#include <vector>
+#include <queue>
 #include <utility>
 #include <memory>
 
@@ -31,10 +31,11 @@ public:
   using item_t = std::shared_ptr<self_t>;
   using feeder_t = tbb::parallel_do_feeder<item_t>;
   
-  static const int threshold = 0;
+  // "magic-constant" - not really: determined by measuring :)
+  static constexpr std::size_t granularity = 16;
 
   Task(at_type at) {
-    _atvec.push_back(std::move(at));
+    add_task(std::move(at));
   }
   
   Task (Task const&) = delete;
@@ -48,11 +49,11 @@ public:
   }
   
   void add_task(at_type at) {
-    _atvec.push_back(std::move(at));
+    _atvec.push(std::move(at));
   }
   
   void add_task(dt_type dt) {
-    _dtvec.push_back(std::move(dt));
+    _dtvec.push(std::move(dt));
   }
 
   template <class ATCIHandler, class DTCIHandler>
@@ -61,24 +62,24 @@ public:
     item_t share_task(new self_t());
     
     auto ath = [this, &share_task] (at_type at) {
-      self_t * task = (weight() > threshold) ? share_task.get() : this;
+      self_t * task = (weight() > granularity) ? share_task.get() : this;
       task->add_task(std::move(at));
     };
     auto dth = [this, &share_task] (dt_type dt) {
-      self_t * task = (weight() > threshold) ? share_task.get() : this;
+      self_t * task = (weight() > granularity) ? share_task.get() : this;
       task->add_task(std::move(dt));
     };
 
     work:
     while (true) {
-      if (share_task->weight() > threshold) {
+      if (share_task->weight() > granularity) {
         f.add(share_task);
         share_task.reset(new self_t());
       }
-      if (not _dtvec.empty())  // TODO measure depth-first / vs. breadt-first
-        process_task(_dtvec, ath, dth, fc, dcih);
-      else if (not _atvec.empty())
+      if (not _atvec.empty())
         process_task(_atvec, ath, dth, fc, acih);
+      else if (not _dtvec.empty())  
+        process_task(_dtvec, ath, dth, fc, dcih);
       else
         break;
     }
@@ -91,14 +92,14 @@ private:
   Task() = default;
 
   template <class task_t, class ... Params>
-  void process_task(std::vector<task_t> & tvec, Params & ... params) {
-    auto task(std::move(tvec.back()));
-    tvec.pop_back();
+  void process_task(std::queue<task_t> & tvec, Params & ... params) {
+    auto task(std::move(tvec.front()));
+    tvec.pop();
     task.execute(std::forward<Params &>(params)...);
   }
-
-  std::vector<at_type> _atvec;
-  std::vector<dt_type> _dtvec;
+  // queues: breadth-first search
+  std::queue<at_type> _atvec;
+  std::queue<dt_type> _dtvec;
 };
 
 }
